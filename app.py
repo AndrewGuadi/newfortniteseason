29dbcac150fd38ae3bae198f8d8f5e12f44757f2
Andrew Guadí <94252760+AndrewGuadi@users.noreply.github.com>
2024-05-21 19:15:12 +0000

    added sitemap and proper if/main

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
