0328faa8de0d76f46bdc32e56c8beeaafd16208a
Andrew Guadí <94252760+AndrewGuadi@users.noreply.github.com>
2024-05-23 01:19:49 +0000

    added the 6 other pages

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template, send_from_directory
-
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+@app.route('/theme')
+def theme():
+    return render_template('theme.html')
 
-@app.route('/sitemap.xml')
-def sitemap():
-    return send_from_directory('static', 'sitemap.xml')
+@app.route('/skins')
+def skins():
+    return render_template('skins.html')
 
+@app.route('/vehicles')
+def vehicles():
+    return render_template('vehicles.html')
 
-if __name__=="__main__":
+@app.route('/emotes')
+def emotes():
+    return render_template('emotes.html')
+
+@app.route('/guests')
+def guests():
+    return render_template('guests.html')
+
+@app.route('/epic')
+def epic():
+    return render_template('epic.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
